adee56c429817fcf5dbdaab70b7bb9aaa004388d
Cian Laguesma <cbalaguesma1101@gmail.com>
2024-03-04 15:32:05 +0800

    Feature: Added simple acceleration for forward and backward

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,18 @@ use bevy::{
     prelude::*,
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
+
+#[derive(Component)]
+struct Player {
+    velocity: Vec2,
+    horizontal_acceleration: f32,
+}
+
+struct Wall{
+    position: Vec2,
+    width: f32,
+    height: f32,
+}
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
@@ -16,12 +28,11 @@ enum Direction {
     Down,
 }
 
-const X_EXTENT: f32 = 600.;
 
+const X_EXTENT: f32 = 600.;
+const X_DECAY: f32 = 1.;
 fn setup(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
 
@@ -34,68 +45,32 @@ fn setup(
             transform: Transform::from_xyz(300.,0.,0.),
             ..default()
         },
-        Direction::Up,
-    ));
-    let shapes=[
-        Mesh2dHandle(meshes.add(Circle { radius: 50.0 })),
-        Mesh2dHandle(meshes.add(Ellipse::new(25.0, 50.0))),
-        Mesh2dHandle(meshes.add(Capsule2d::new(25.0, 50.0))),
-        Mesh2dHandle(meshes.add(Rectangle::new(50.0, 100.0))),
-        Mesh2dHandle(meshes.add(RegularPolygon::new(50.0, 6))),
-    ];
-
-    let num_shapes = shapes.len();
-
-    for(i, shape) in shapes.into_iter().enumerate(){
-        let color = Color::hsl(360. * i as f32/num_shapes as f32, 0.95, 0.7);
-
-        commands.spawn((MaterialMesh2dBundle{
-            mesh:shape,
-            material: materials.add(color),
-            transform: Transform::from_xyz(
-                -X_EXTENT /2. + i as f32 / (num_shapes -1) as f32 * X_EXTENT,
-                0.0,
-                0.0,
-            ),
-            ..default()
+        Player{
+            velocity: Vec2 {x:0., y:0.},
+            horizontal_acceleration: 1.,
         },
-        Direction::Up,
-        ));
-    }
+    ));
 }
 
-fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
-    for (mut logo, mut transform) in &mut sprite_position {
-        match *logo {
-            Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
-            Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
-        }
-
-        if transform.translation.y > 200. {
-            *logo = Direction::Down;
-        } else if transform.translation.y < -200. {
-            *logo = Direction::Up;
-        }
-    }
-}
-
-fn keyboard_input_system(keyboard_input: Res<ButtonInput<KeyCode>>, mut sprite_position: Query<(&mut Direction, &mut Transform)>, time: Res<Time>){
-    for (mut logo, mut transform) in &mut sprite_position {
+fn keyboard_input_system(keyboard_input: Res<ButtonInput<KeyCode>>, mut sprite_position: Query<(&mut Transform, &mut Player, &Velocity, &Acceleration)>, time: Res<Time>){
+    for (mut transform, mut player, velocity, acceleration) in &mut sprite_position {
+        // Implement jump frames when space is pressed
         if keyboard_input.pressed(KeyCode::KeyW){
-            transform.translation.y += 150. * time.delta_seconds();
+            player.velocity.y += velocity.y * time.delta_seconds();
         }
         if keyboard_input.pressed(KeyCode::KeyS){
-    
-            transform.translation.y -= 150. * time.delta_seconds();
+            player.velocity.y = velocity.y * time.delta_seconds() + -0.5 * acceleration.0 * (time.delta_seconds()).powf(2.0);
         }
         if keyboard_input.pressed(KeyCode::KeyA){
             
-            transform.translation.x -= 150. * time.delta_seconds();
+            player.velocity.x -= player.horizontal_acceleration * time.delta_seconds();
         }
         if keyboard_input.pressed(KeyCode::KeyD){
             
-            transform.translation.x += 150. * time.delta_seconds();
+            player.velocity.x += player.horizontal_acceleration * time.delta_seconds();
         }
+
+        transform.translation.x += player.velocity.x;
     }
     
 }
